dd726f1e4a94935e5cbca20cb111606bd2170145
leeyuujin <lyjyj061026@gmail.com>
2026-06-03 15:08:21 +0900

    Add files via upload

--- Vec.h
+++ Vec.h
@@ -11,14 +11,14 @@ using namespace std;
 
 template <class T> class Vec {
 public:
-	//속성
+	// 속성
 	typedef T* iterator;
 	typedef const T* const_iterator;
 	typedef size_t size_type;
 	typedef T value_type;
-	typedef ptrdiff_t difference_type;
-	typedef T& reference;
-	typedef const T& const_reference;
+	typedef ptrdiff_t difference_type; // *i - *j
+	typedef T& reference; // &
+	typedef const T& const_reference; // const &
 
 	/*
 	Vec<int> grades;
@@ -28,42 +28,50 @@ public:
 	2. Vec() 사본 만든 생성자
 	*/
 
+
 	// 생성자 (여러가지)
 	// 1. 빈 객체
-	Vec() { create(); };
-
+	Vec() { create(); }; 
 	// 2. 벡터 크기와 선택적 초기값 정의하기
 	explicit Vec(size_type n, const T& val = T()) { create(n, val); }
-
 	// 3. 사본 만드는 생성자
 	Vec(const Vec& v) { create(v.begin(), v.end()); };
+	// 4. Template 추가
+	template <class In> Vec(In b, In e) {
+		create(b, e);
+	}
 
 	// 벡터 크기와 인덱스에 관한 새로운 함수들
-	size_type size() const { return avail - data; }
+	size_type size() const { return avail - data; } // 수정
+	T& operator[](size_type i) { return data[i]; }
 	const T& operator[](size_type i) const { return data[i]; }
 
 	Vec& operator=(const Vec&);
 
-	//	반복자를 반환하는 새로운 함수
+	// 반복자를 반환하는 새로운 함수
 	iterator begin() { return data; }
 	const_iterator begin() const { return data; }
 
-	iterator end() { return avail; }
-	const_iterator end() const { return avail; }
-	
-	// 인터페이스
-		void push_back(const T & val) {
-			if (avail == limit) // 필요하다면 저장 공간 확보
-				grow();
+	iterator end() { return avail; } // 수정
+	const_iterator end() const { return avail; } // 수정
 
-			unchecked_append(val); // 새로운 요소 추가
-			
-		}
+	// insert & assign 추가 (연습문제 12 + 13)
+	template<class In> iterator insert(iterator, In, In); // Q12
+	void assign(iterator, iterator); // Q13 assign is = 할당/정의
+
+	// 인터페이스 = 미완성...
+	void push_back(const T& val) {
+		if (avail == limit) // 필요하다면 자장 공간 확보
+			grow();
+
+		unchecked_append(val); // 새로운 요소 추가
+	}
+
 	// Q9을 위해 추가
-		void push_back_grow_once(const T& t) {
-			grow_once();
-			unchecked_append(t);
-		}
+	void push_back_grow_once(const T& t) {
+		grow_once();
+		unchecked_append(t);
+	}
 
 	// 소멸자
 	~Vec() { uncreate(); };
@@ -71,7 +79,7 @@ public:
 	bool empty() const { return data == avail; } // 0 == 0? True
 
 private:
-	iterator data; // Vec의 첫 번째 요소를 가리키는 포인터
+	iterator data;  // Vec의 첫 번째 요소를 가리키는 포인터
 	iterator avail; // Vec의 마지막 요소 다음을 가리키는 포인터
 	iterator limit; // 사용할 수 있는 마지막 요소의 다음을 가리키는 포인터
 
@@ -80,7 +88,7 @@ private:
 	// 멤버 함수
 	// 1. 생성자: 메모리 할당 + 값 초기화 함수
 	void create();
-	void create(size_type, const T&);
+	void create(size_type, const T&); 
 	void create(const_iterator, const_iterator);
 
 	// 2. 소멸자: 메모리 해제
@@ -88,42 +96,47 @@ private:
 
 	// 3. 유틸리티 함수들 (push_back의 지원 함수)
 	void grow();
-	void grow_once();
+	void grow_once(); // Q9을 위해 추가
 	void unchecked_append(const T&);
-
 };
 
 template <class T> Vec<T>& Vec<T>::operator=(const Vec& rhs) {
 	// int i = 0;
-	// lhs = left-hand side (i) = 변수
+	// lhs = left-hand side  (i) = 변수
 	// rhs = right-hand side (0) = 값
 	// 자가 할당 여부 확인
 	if (&rhs != this) {
 		// 좌변이 지닌 배열이 점유하는 메모리를 해제
 		uncreate();
 
-		// 우변이 지닌 요소를 좌변으로 복사
+		// 우변이 지닌 요소를 좌변으로 복사 
 		create(rhs.begin(), rhs.end());
-
 	}
 	return *this;
 }
 
-template <class T> void Vec<T>::create() {
+// 1. 메모리를 할당하고 요소를 초기화
+template <class T>
+void Vec<T>::create() {
 	data = avail = limit = 0;
 }
 
-template <class T> void Vec<T>::create(size_type n, const T& val) {
-	data = alloc.allocate(); 
+// 2. 다양한 버전이 존재하며, 각 함수는 
+// 할당된 메모리를 초기화하고
+// 포인터를 적절히 설정합니다.
+template <class T>
+void Vec<T>::create(size_type n, const T& val) {
+	data = alloc.allocate(n); // #include <memory> <<< 수정 >>>
 	limit = avail = data + n;
-	uninitialized_fill(data, limit, val); // 작성
+	uninitialized_fill(data, limit, val); // 작성...
 }
 
+// 3. limit와 avail은 항상 같은 메모리 주소를
+// 가리키게 됩니다.
 template <class T>
-void Vec<T>::create(const_iterator i , const_iterator j) {
+void Vec<T>::create(const_iterator i, const_iterator j) {
 	data = alloc.allocate(j - i);
-	limit = avail = uninitialized_copy(i, j, data); // 작성
-
+	limit = avail = uninitialized_copy(i, j, data); // 작성...
 }
 
 template <class T>
@@ -131,7 +144,7 @@ void Vec<T>::uncreate() {
 	if (data) {
 		// 역순서대로 모든 요소를 소멸하고 메모리 해제
 		iterator it = avail; // 마지막 요소 다음에 가리키는 포인터
-		while (it != data) // 첫 번째 요소를 가리키는 포인터까지
+		while (it != data) // 첫 번째 요로를 가리키는 포인터까지
 			alloc.destroy(--it);
 
 		// 할당한 메모리 공간을 해제 (비우기)
@@ -153,38 +166,73 @@ template <class T>
 void Vec<T>::grow() {
 	size_type new_size = max(2 * (limit - data), ptrdiff_t(1));
 
-	// 새로운 공간 할당하고 모든 있는 요소를 복사
+	// 새오운 공간 할당하고 모든 있는 요소를 복사
 	iterator new_data = alloc.allocate(new_size); // 할당
 	iterator new_avail = uninitialized_copy(data, avail, new_data);
 
 	// 구공간을 소멸
 	uncreate();
 
-	// 모든 포인터를 새로운 공간에서 가리킴
+	// 모든 포인터를 새로운 공간에서 가리겨
 	data = new_data;
 	avail = new_avail;
 	limit = data + new_size;
-
 }
 
-//Q9을 위해 추가
-
+// Q9 위해 추가
 template <class T>
 void Vec<T>::grow_once() {
 	size_type new_size = limit - data + ptrdiff_t(1);
 
-	// 새로운 공간 할당하고 모든 있는 요소를 복사
+	// 새오운 공간 할당하고 모든 있는 요소를 복사
 	iterator new_data = alloc.allocate(new_size); // 할당
 	iterator new_avail = uninitialized_copy(data, avail, new_data);
 
 	// 구공간을 소멸
 	uncreate();
 
-	// 모든 포인터를 새로운 공간에서 가리킴
+	// 모든 포인터를 새로운 공간에서 가리겨
 	data = new_data;
 	avail = new_avail;
 	limit = data + new_size;
+}
 
+// 12장 - 연습문제 12
+template <class T>
+template <class In>
+typename Vec<T>::iterator Vec<T>::insert(iterator d, In b, In e) {
+	// 끝 부분과 차이 저장
+	Vec<T> end(d, avail); // d부터 끝까지 다른 Vec에서 data 저장
+	ptrdiff_t diff = d - data;
+
+	// 끝 부분 소멸하기
+	if (d) {
+		// 역방향으로 d까지 요소를 제거/삭제하기
+		while (avail != d)
+			alloc.destroy(--avail);
+	}
+
+	// b부터 e까지 요소를 추가하기
+	while (b != e)
+		push_back(*b++);
+
+	// 다시 끝 부분 추가하기 (불러오기)
+	for (typename Vec<T>::const_iterator it = end.begin();
+		it != end.end(); ++it)
+		push_back(*it);
+
+	return data + diff;
+}
+
+// 12장 - 연습문제 13
+template <class T>
+void Vec<T>::assign(iterator b, iterator e) {
+	// Vec 소멸
+	uncreate();
+
+	// 모든 반복자의 요소를 추가하기
+	while (b != e)
+		push_back(*b++);
 }
 
 #endif
